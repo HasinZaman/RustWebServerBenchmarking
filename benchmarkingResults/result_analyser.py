95f4cb847c3c5523b58034f8d10eef00b676a0be
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2023-07-23 10:08:37 -0400

    Create result_analyser.py

---
+++ benchmarkingResults/result_analyser.py
@@ -0,0 +1,14 @@
+# segregates all text files by
+# - test type
+# - test group
+# - test backend
+
+# Get Stats
+# - get median
+# - get lower bound
+# - get upper bound
+
+# Graph data
+# - Graph by type
+# - Graph by group
+# - waterfall graph of memory consumption with scatter graph on top
